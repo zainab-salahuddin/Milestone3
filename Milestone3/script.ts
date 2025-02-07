a477c03f3e6c1ecdac7fc65f3ce767c8f78fb505
zainab-salahuddin <162922997+zainab-salahuddin@users.noreply.github.com>
2025-02-07 08:56:58 +0500

    Add files via upload

---
+++ Milestone3/script.ts
@@ -0,0 +1,51 @@
+document.getElementById('resumeForm')?.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Get references to form elements using their IDs
+    
+    const nameElement = document.getElementById('name');
+    const emailElement = document.getElementById('email');
+    const phoneElement = document.getElementById('phone');
+    const educationElement = document.getElementById('education');
+    const experienceElement = document.getElementById('experience');
+    const skillsElement = document.getElementById('skills');
+
+    if (nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
+
+        const name = (nameElement as HTMLInputElement).value;
+        const email = (emailElement as HTMLInputElement).value;
+        const phone = (phoneElement as HTMLInputElement).value;
+        const education = (educationElement as HTMLInputElement).value;
+        const experience = (experienceElement as HTMLInputElement).value;
+        const skills = (skillsElement as HTMLInputElement).value;
+
+
+        // Create resume output
+        const resumeOutput = `
+        <h2>Resume</h2>
+    
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone Number:</strong> ${phone}</p>
+        
+
+        <h3>Education</h3>
+        <p>${education}</p>
+
+        <h3>Work Experience</h3>
+        <p>${experience}</p>
+
+        <h3>Skills</h3>
+        <p>${skills}</p>
+        `;
+
+        const resumeOutputElement = document.getElementById('resumeOutput');
+        if (resumeOutputElement) {
+            resumeOutputElement.innerHTML = resumeOutput;
+        } else {
+            console.error('The resume output element is missing');
+        }
+    } else {
+        console.error('One or more form elements are missing');
+    }
+});
